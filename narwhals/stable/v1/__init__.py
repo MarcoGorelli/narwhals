86e9d474097b63346028f26a8581a7cc57758ae0
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-03-11 13:11:53 +0000

    note what happens if dtype/schema not passed

--- narwhals/stable/v1/__init__.py
+++ narwhals/stable/v1/__init__.py
@@ -2228,7 +2228,8 @@ def from_dict(
 
     Arguments:
         data: Dictionary to create DataFrame from.
-        schema: The DataFrame schema as Schema or dict of {name: type}.
+        schema: The DataFrame schema as Schema or dict of {name: type}. If not
+            specified, the schema will be inferred by the native library.
         backend: specifies which eager backend instantiate to. Only
             necessary if inputs are not Narwhals Series.
 
