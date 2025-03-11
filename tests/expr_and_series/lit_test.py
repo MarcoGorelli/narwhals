8dd5f2625683f90902ba8f8935ec62ba4863d6b6
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-03-11 12:22:37 +0000

    test lit and concat_str cases

--- tests/expr_and_series/lit_test.py
+++ tests/expr_and_series/lit_test.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 from typing import Any
 
 import numpy as np
+import pyarrow as pa
 import pytest
 
 import narwhals.stable.v1 as nw
@@ -129,3 +130,15 @@ def test_date_lit(constructor: Constructor, request: pytest.FixtureRequest) -> N
         assert result == {"a": nw.Int64, "literal": nw.Datetime}
     else:
         assert result == {"a": nw.Int64, "literal": nw.Date}
+
+
+def test_pyarrow_lit_string() -> None:
+    df = nw.from_native(pa.table({"a": [1, 2, 3]}))
+    result = df.select(nw.lit("foo")).to_native().schema.field_by_name("literal")
+    assert pa.types.is_large_string(result.type)
+    result = (
+        df.select(nw.lit("foo", dtype=nw.String))
+        .to_native()
+        .schema.field_by_name("literal")
+    )
+    assert pa.types.is_large_string(result.type)
