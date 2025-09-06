42cbe1a02b45e94777564f76a8ba37c496de97ba
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-09-06 17:39:53 +0100

    wip

--- narwhals/dataframe.py
+++ narwhals/dataframe.py
@@ -159,11 +159,11 @@ class BaseFrame(Generic[_FrameT]):
         for expr in flatten(exprs):
             compliant_expr = self._extract_compliant(expr)
             out_exprs.append(compliant_expr)
-            out_kinds.append(ExprKind.from_expr(compliant_expr))
+            out_kinds.append(ExprKind.from_into_expr(compliant_expr, str_as_lit=False))
         for alias, expr in named_exprs.items():
             compliant_expr = self._extract_compliant(expr)
             out_exprs.append(compliant_expr.alias(alias))
-            out_kinds.append(ExprKind.from_expr(compliant_expr))
+            out_kinds.append(ExprKind.from_into_expr(compliant_expr, str_as_lit=False))
         return out_exprs, out_kinds
 
     @abstractmethod
