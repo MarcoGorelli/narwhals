a526899ae432227801882f7e971f69f6727fa6c2
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-09-22 15:48:21 +0300

    bit more

--- narwhals/_sql/namespace.py
+++ narwhals/_sql/namespace.py
@@ -80,10 +80,10 @@ class SQLNamespace(
         then: SQLExprT | NonNestedLiteral,
         otherwise: SQLExprT | NonNestedLiteral | None = None,
     ) -> SQLExprT:
-        then_ce: SQLExprT = (
+        then_ce = (
             then if is_compliant_expr2(then) else self.lit(then, None)  # type: ignore[arg-type]
         )
-        otherwise_ce: SQLExprT | None = (
+        otherwise_ce = (
             otherwise
             if is_compliant_expr2(otherwise)
             else None
